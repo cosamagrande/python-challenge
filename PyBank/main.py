72f7219c83098928957a9ddde3da07ca905ac314
cosamagrande <54606354+cosamagrande@users.noreply.github.com>
2019-10-26 16:52:43 -0500

    Version with average profit loss calculated

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,31 +5,57 @@ import csv
 
 csvpath = os.path.join('..','Resources','budget_data.csv')
 
-#creating lists
-Monthbucket = []
-Prf_loss = []
 # Reading CSV file
-with open (csvpath,newline='') as csvfile:
+with open (csvpath,'r',newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 #    Skipping Headers on file
     csv_header = next(csvreader)
 
+    #creating lists
+    MonthList = []
+    Prf_lossList = []
+
 # iterating over rows to count for # of months and Total Profit/loss
     for row in csvreader:
         Months = row[0]
-        Monthbucket.append(Months)
+        MonthList.append(Months)
 
-        suma = row[1]
-        Prf_loss.append(suma)
+        suma = int(row[1])
+        Prf_lossList.append(suma)
 
 # printing results
 print('Financial Analyses')
 print('------------------')
-print(f'Total Months: {len(Monthbucket)}')
-print(f'Total: ${sum(map(int,Prf_loss))}')
+print(f'Total Months: {len(MonthList)}')
+print(f'Total: ${sum(map(int,Prf_lossList))}')
+
+#Generating successive element difference within Prf_Loss list
+
+Month_Diff = [Prf_lossList[i + 1] - Prf_lossList[i] for i in range(len(Prf_lossList)-1)] 
+
+#Calculating average of Month_Diff
+
+def Ave(Month_Diff):
+    return sum(Month_Diff)/len(Month_Diff)
+average = Ave(Month_Diff)
+
+# printing results
+print(f'Average Change: ${round(average,2)}')
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 
 
